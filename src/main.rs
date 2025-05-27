b855de14fbdd06b0f1cdeecdd87da82ad963bb2b
mo <pitipon@gmail.com>
2025-05-27 09:54:13 +0800

    translate to pinyin

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ async fn main() -> Result<(), anyhow::Error> {
     println!("############# APP START #############");
     println!("Load Whisper model ...");
     // Create a new whisper model.
-    let model = Whisper::new().await?;
-
     let whisper_model = WhisperBuilder::default()
         .with_language(Some(WhisperLanguage::Chinese)) // Specify Japanese
         .build()
@@ -23,14 +21,14 @@ async fn main() -> Result<(), anyhow::Error> {
         .await?;
     let llama_chat_template = llama_model.chat().with_system_prompt(SYSTEM_PROMPT);
 
-    println!("All models loaded. Listening for microphone input...");
+    println!("✅ Online! Listening for microphone input...");
 
     // Stream audio from the microphone
     let mic = MicInput::default();
     let vad_stream = mic.stream().voice_activity_stream();
     let mut audio_chunks = vad_stream
         .inspect(move |vad_output| {
-            let samples_count = vad_output.samples.clone().count();
+            let _samples_count = vad_output.samples.clone().count();
             // if samples_count > 0 {
             //     println!("samples_count : {samples_count}");
             // }
@@ -61,15 +59,26 @@ async fn main() -> Result<(), anyhow::Error> {
                 println!("Transcribed: {}", current_segment_text.clone());
             }
         }
+
+        // Translate to pinyin
+        let llama_chat_template_to_translate = llama_chat_template.clone();
+        let current_segment_text_to_translate = current_segment_text.clone();
+
+        let translation_task = tokio::spawn(async move {
+            let prompt = format!(
+                "Translate the following Chinese text to Pinyin with English, Output only the English translation. Do not add any pleasantries or extra explanations. Do not translate English, keep as is.:\n{}",
+                current_segment_text_to_translate
+            );
+
+            let mut llama_chat = llama_chat_template_to_translate;
+            let mut response_stream = llama_chat(&prompt);
+            let raw_translation = response_stream.all_text().await;
+            println!("[Pinyin]: {}", raw_translation);
+        });
+
+        // Wait for the translation task to finish before processing the next
+        translation_task.await?;
     }
-
-    let stream = mic.stream();
-
-    // Transcribe the audio into text in chunks based on voice activity.
-    let mut text_stream = stream.transcribe(model);
-
-    // Finally, print the text to the console
-    text_stream.to_std_out().await.unwrap();
 
     Ok(())
 }
